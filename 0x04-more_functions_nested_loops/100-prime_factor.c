3c770ee60ef2b4c66139fdfd1efa23dae52b1275
JajaAli <jhadevonkamon@gmail.com>
2023-02-23 16:59:25 -0800

    loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -18,22 +18,21 @@ int main(void)
 	while (n % 2 == 0)
 	{
 		max = 2;
-		n / = 2;
+		n /= 2;
 	}
 
 	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
-		while (n % i == 0)
-		{
-			max = i;
-			n = n / i;
-		}
+	while (n % i == 0)
+	{
+		max = i;
+		n = n / i;
+	}
 	}
 
 	if (n > 2)
-		max = n;
-
-	printf("%Id\n", max);
+	max = n;
+	printf("%lid\n", max);
 
 	return (0);
 }
